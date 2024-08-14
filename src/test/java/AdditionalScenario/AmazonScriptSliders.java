4e496c7679f09fc07645ae1e15d46dc9b09073da
chouhanomsingh <moodomsingh@gmail.com>
2024-08-14 10:14:29 +0530

    Update AmazonScriptSliders.java

--- src/test/java/AdditionalScenario/AmazonScriptSliders.java
+++ src/test/java/AdditionalScenario/AmazonScriptSliders.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//amazon
 public class AmazonScriptSliders {
 
 	public static void main(String[] args) throws InterruptedException {
